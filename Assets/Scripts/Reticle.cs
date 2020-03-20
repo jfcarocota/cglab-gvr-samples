ab03fea254007ba47ff7deccc9b64645048aed90
jfcarocota <jfcarocota@gmail.com>
2020-03-19 17:11:42 -0700

    reticle

---
+++ Assets/Scripts/Reticle.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reticle : MonoBehaviour
+{
+    [SerializeField]
+    Color rayColor = Color.magenta;
+    [SerializeField, Range(0.1f, 100f)]
+    float rayDistance;
+    [SerializeField]
+    LayerMask layer;
+    RaycastHit hit;
+
+    [SerializeField]
+    Transform reticle;
+    [SerializeField]
+    Vector3 startSize;
+    [SerializeField]
+    float error;
+
+    void Start()
+    {
+        startSize = reticle.localScale;
+    }
+    
+    void FixedUpdate()
+    {
+        if(Physics.Raycast(transform.position, transform.forward, out hit, rayDistance, layer))
+        {
+            //Debug.Log("hit");
+            //Debug.Log($"distance:{hit.distance}");
+            //Debug.Log($"hit point:{hit.point}");
+            //Debug.Log($"normal: {hit.normal}");
+            
+            reticle.localScale = startSize * error * hit.distance;
+            reticle.position = hit.point + new Vector3(0, 0, -0.01f);
+            reticle.localRotation = Quaternion.LookRotation(hit.normal);
+        }
+        else
+        {
+            reticle.localPosition = Vector3.zero;
+            reticle.localRotation = Quaternion.identity;
+            reticle.localScale = startSize;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = rayColor;
+        Gizmos.DrawRay(transform.position, transform.forward * rayDistance);
+    }
+}
